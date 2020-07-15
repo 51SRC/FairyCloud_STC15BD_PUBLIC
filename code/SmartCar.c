3910fa9081c33b35b27266ebf3574dde68babd9a
TianYuan <yuan.tian@timanetworks.com>
2020-07-15 12:31:23 +0800

    调试屏幕，能正常显示数据了

--- code/SmartCar.c
+++ code/SmartCar.c
@@ -33,7 +33,7 @@ typedef long I32;
 typedef unsigned char U8; 
 
 U8 SRCHeader = 0x23;
-U8 xdata SRCCID[] = {"SRC00000000000004"};
+U8 xdata SRCCID[] = {"SRC00000000000001"};
 U8 xdata netConfig[] = "AT+CWJAP=\"Gunter\",\"{qwerty123}\"\r\n\0";
 U8 xdata DATA_GET[500]={0};//缓冲区长度
 
@@ -136,33 +136,48 @@ void LEDFunc(void)	{
 //		DELAY_MS(2000);
 //		OLED_Fill(0x00); //屏全灭
 //		DELAY_MS(200);
-		OLED_P16x16Ch(24,0,1);
-		OLED_P16x16Ch(40,0,2);
-		OLED_P16x16Ch(57,0,3);
-		OLED_P16x16Ch(74,0,4);
-		OLED_P16x16Ch(91,0,5);
+
+
+//		OLED_P16x16Ch(24,0,1);
+//		OLED_P16x16Ch(40,0,2);
+//		OLED_P16x16Ch(57,0,3);
+//		OLED_P16x16Ch(74,0,4);
+//		OLED_P16x16Ch(91,0,5);
+
 		for(i=0; i<8; i++)//通过点整显示汉字 -- i表示字表数组的位置
 		{
-//			OLED_P16x16Ch(i*16,0,i);
+			OLED_P16x16Ch(i*16,0,i);
 		 	OLED_P16x16Ch(i*16,2,i+8);
 		 	OLED_P16x16Ch(i*16,4,i+16);
 		 	OLED_P16x16Ch(i*16,6,i+24);
 		}
-		DELAY_MS(4000);
-		OLED_CLS();//清屏
-
-		OLED_P8x16Str(0,0,"HelTec");//第一行 -- 8x16的显示单元显示ASCII码
-		OLED_P8x16Str(0,2,"OLED Display");
-		OLED_P8x16Str(0,4,"www.heltec.cn");
-		OLED_P6x8Str(0,6,"cn.heltec@gmail.com");
-		OLED_P6x8Str(0,7,"heltec.taobao.com");
-		DELAY_MS(4000);
-		OLED_CLS();
-
-		Draw_BMP(0,0,128,8,BMP1);  //图片显示(图片显示慎用，生成的字表较大，会占用较多空间，FLASH空间8K以下慎用)
 		DELAY_MS(8000);
-		Draw_BMP(0,0,128,8,BMP2);
-		DELAY_MS(8000);
+
+
+
+		
+		for(i=0; i<8; i++)//通过点整显示汉字 -- i表示字表数组的位置
+		{
+			OLED_P16x16Ch(i*16,0,i);
+		 	OLED_P16x16Ch(i*16,2,i+8);
+		 	OLED_P16x16Ch(i*16,4,i+16);
+		 	OLED_P16x16Ch(i*16,6,i+24);
+		}
+	//	DELAY_MS(4000);
+//		OLED_CLS();//清屏
+
+//		OLED_P8x16Str(0,0,"温度：12℃");//第一行 -- 8x16的显示单元显示ASCII码
+//		OLED_P8x16Str(0,2,"OLED Display");
+//		OLED_P8x16Str(0,4,"www.heltec.cn");
+//		OLED_P6x8Str(0,6,"cn.heltec@gmail.com");
+//		OLED_P6x8Str(0,7,"heltec.taobao.com");
+//		DELAY_MS(4000);
+	//	OLED_CLS();
+
+//		Draw_BMP(0,0,128,8,BMP1);  //图片显示(图片显示慎用，生成的字表较大，会占用较多空间，FLASH空间8K以下慎用)
+//		DELAY_MS(8000);
+//		Draw_BMP(0,0,128,8,BMP2);
+//		DELAY_MS(8000);
 	}
 
 unsigned char CheckBCC(unsigned char len, unsigned char *recv){
